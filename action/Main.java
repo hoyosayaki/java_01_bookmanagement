eb21f2bd9002f0a07729c110e10acbeb95082355
s <kiya.sayoho@gmail.com>
2020-02-08 17:40:44 +0900

    図書管理プログラム ver1.0

---
+++ action/Main.java
@@ -0,0 +1,17 @@
+package action;
+
+
+import java.sql.SQLException;
+
+import screen.Top;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		// ‚±‚±‚ÉƒR[ƒh‚ğ‘}“ü
+
+		Top top = new Top();
+		top.Top();
+
+	}
+}
